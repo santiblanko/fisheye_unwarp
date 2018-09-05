65c1908c4483d900d584d8d1f4a4674524595456
cool-benson <coolbenson.chang@gmail.com>
2018-09-04 21:20:31 -0700

    add find checkerboard and fisheye calibration

---
+++ find_checkerboard.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2 as cv
+
+i = 0
+
+def find_checkerboard(mask_rect, image, pattern_size = (6, 3)):
+    """
+    This function finds points on checkerbord. 
+
+    Args:
+      mask_rect: (x,y,w,h), Part of the image that is used to find checkerboard,\
+      x,y is the top left start point. w,h is the width and height of the image.
+      image: Gray scale image
+      pattern_size: The size of checker board.
+
+    Return:
+      corners: The founded checker board. \
+      Note, if mask is set than the corners is compensated for masking.
+    """
+    global i
+    img_h,img_w = image.shape[:2]
+
+    subImage = image[mask_rect[1]:min(mask_rect[1]+mask_rect[3],img_h),\
+                     mask_rect[0]:min(mask_rect[0]+mask_rect[2],img_w)]
+    pattern_points = np.zeros((np.prod(pattern_size), 3), np.float32)
+    pattern_points[:, :2] = np.indices(pattern_size).T.reshape(-1, 2)
+
+    #find corners
+    found, corners = cv.findChessboardCorners(subImage, pattern_size)
+
+    #if found refine by subpix
+    #cv.imshow("aaa", subImage)
+    #cv.waitKey(3000)
+    if found:
+        term = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_COUNT, 30, 0.1)
+        cv.cornerSubPix(subImage, corners, (5, 5), (-1, -1), term)
+        vis = cv.cvtColor(subImage, cv.COLOR_GRAY2BGR)
+        
+        cv.drawChessboardCorners(vis, pattern_size, corners, found)
+        #cv.imshow("aaa", vis)
+        cv.imwrite('checkerboard_{0}.png'.format(i), vis)
+        i+= 1
+        
+        corners[:,:,0:2] += mask_rect[:2]
+        return corners
+    else:
+        return None
+
+if __name__ == "__main__":
+
+    image = cv.imread("fisheye_l.jpg")
+    h,w = image.shape[:2]
+    gray_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    find_checkerboard(np.array([int(w/2.0),int(h/2.0),int(w/2.0),int(h/2.0)]),gray_img )
--- fisheye_unwarp.py
+++ fisheye_unwarp.py
@@ -6,6 +6,11 @@ import cv2
 import numpy
 import numpy.linalg
 import math
+import find_checkerboard
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
 
 PI = math.pi
 
@@ -117,8 +122,136 @@ def test_fisheye_to_equ():
 
 
 
+def dual_fisheye_calibrate(image_l, image_r,fov_l,fov_r):
+
+    # for both image
+    #[1/2.0,0.0,1/2.0,1/2.0],[1/4.0,0.0,1/4.0,1/4.0],[1/4.0,3/4.0,1/4.0,1/4.0],[1/2.0,3/4.0,1/4.0,1/4.0]
+    #[1/4.0,0.0,1/4.0,1/4.0],[1/2.0,0.0,1/4.0,1/4.0],[1/2.0,3/4.0,1/4.0,1/4.0],[1/4.5,3/4.0,1/4.0,1/4.0]
+    image_boards = numpy.array([[[1/2.0,0.0,1/2.0,1/2.0],[1/4.0,0.0,1/4.0,1/4.0],[1/4.0,3/4.0,1/4.0,1/4.0],[1/2.0,3/4.0,1/4.0,1/4.0]],\
+                                [[1/4.0,0.0,1/4.0,1/4.0],[1/2.0,0.0,1/4.0,1/4.0],[1/2.0,3/4.0,1/4.0,1/4.0],[1/4.5,3/4.0,1/4.0,1/4.0]]])
+    all_points = []
+    ite = 100
+
+    
+    # 1. find points
+    for image, boards in zip([image_l,image_r],image_boards):
+        h,w = image.shape[:2]
+        length = max(w,h)
+
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        board_points = numpy.zeros((0,2))
+        for board in boards:
+            mask = numpy.array([board[0]*w,board[1]*h,board[2]*w,board[3]*h], dtype=numpy.int32)
+            point = find_checkerboard.find_checkerboard(mask,gray_img)
+            point = numpy.squeeze(point, axis = 1)
+            #print board_points.shape, point.shape,numpy.tile([1.0,1.0],(len(board_points),2)).shape
+            board_points = numpy.vstack((board_points, point))
+        board_points /= length/2.0
+        #print numpy.tile([1.0,1.0],(len(board_points),2))
+        board_points -= numpy.tile([1.0,1.0],(len(board_points),1))
+        all_points.append(board_points)
+
+    # 2. unwarp and project to global pxyz
+    def find_r_t(fov_l,fov_r):
+        rotation_matrix_l = numpy.eye(3)
+        rotation_matrix_r = numpy.eye(3)
+        r = 1000
+        det_y_l = numpy.array([0.0,0.0,0.0])
+        det_y_r = numpy.array([0.0,0.0,0.0])
+        ite = 10
+        while ite > 0:
+            l_reproject_pts = reproject_coords(all_points[0],fov_l, r, det_y_l, rotation_matrix_l)
+            r_reproject_pts = reproject_coords(all_points[1],fov_r, r, det_y_r, rotation_matrix_r)
+            error =  calculate_error(numpy.eye(3), numpy.zeros((1,3)),l_reproject_pts,r_reproject_pts)
+
+           
+
+            # 3. find translation + rotation for points
+            new_rotation,t = find_rigid_transformation(numpy.mat(l_reproject_pts),numpy.mat(r_reproject_pts))
+
+            # 4. calculate error
+            #print rotation_matrix_l
+
+            det_y_l += numpy.array(t)[:,0]/2.0
+            det_y_r -= numpy.array(t)[:,0]/2.0
+
+            numpy.matmul(new_rotation,rotation_matrix_l,rotation_matrix_l)
+
+            #print calculate_error(new_rotation, t,l_reproject_pts,r_reproject_pts)
+            ite -= 1
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(l_reproject_pts[:,0], l_reproject_pts[:,1], l_reproject_pts[:,2],c='r')
+        ax.scatter(r_reproject_pts[:,0], r_reproject_pts[:,1], r_reproject_pts[:,2],c='b')
+        plt.show()
+        return (error,rotation_matrix_l.T,det_y_l , rotation_matrix_r.T, det_y_r)
+    min_value = float("inf")
+    min_result = None
+    min_fov = None
+    for d_fov_l in xrange(-1,1,1):
+        for d_fov_r in xrange(-1,1,1):
+            try:
+                result = find_r_t(fov_l+d_fov_l/180.0 * math.pi, fov_r+d_fov_r/180.0 * math.pi)
+            except:
+                continue
+            if result[0] < min_value:
+                min_value = result[0]
+                min_result = result
+                min_fov = (fov_l+d_fov_l/180.0 * math.pi, fov_r+d_fov_r/180.0 * math.pi)
+
+
+
+    return result,min_fov
+
+
+
+def calculate_error(r,t,pts_a,pts_b):
+    diff = 0.0
+    for p_a, p_b in zip(pts_a,pts_b):
+        diff += numpy.linalg.norm(r.dot(p_a)+t-p_b)
+    return diff/len(p_a)
+
+
+def find_rigid_transformation(A, B):
+    assert len(A) == len(B)
+
+    N = A.shape[0]; # total points
+
+    centroid_A = numpy.mean(A, axis=0)
+    centroid_B = numpy.mean(B, axis=0)
+    
+    # centre the points
+    AA = A - numpy.tile(centroid_A, (N, 1))
+    BB = B - numpy.tile(centroid_B, (N, 1))
+
+    # dot is matrix multiplication for array
+    H = numpy.transpose(AA) * BB
+
+    U, S, Vt = numpy.linalg.svd(H)
+
+    R = Vt.T * U.T
+
+    # special reflection case
+    if numpy.linalg.det(R) < 0:
+       print "Reflection detected"
+       Vt[2,:] *= -1
+       R = Vt.T * U.T
+
+    t = -R*centroid_A.T + centroid_B.T
+
+    return R, t
+
+
+def reproject_coords(coords, fov, r, o_center_position, rotation_matrix):
+    result = numpy.zeros((len(coords), 3))
+    for i,fish_coord in enumerate(coords):
+        fi,theta = fisheye_coord_to_fi_theta(fish_coord,fov)
+        # print fi,theta
+        result[i] = fi_theta_to_pxyz(fi,theta,o_center_position,r,rotation_matrix)
+    return result
+
 def vector_to_r_theta_with_delta_y(px,py,pz, aperture, dy, rotation_matrix):
-    vec = numpy.array([px,py-dy,pz])
+    vec = numpy.array([px,py,pz])-dy
     vec = vec/numpy.linalg.norm(vec)
     px,py,pz = rotation_matrix.dot(vec)
 
@@ -150,7 +283,7 @@ def vector_to_fisheye(px,py,pz, aperture, det_y = 0.0, rotation_matrix = numpy.e
     return x,y
 
 
-def generate_map(input_image_size, output_image_size, aperture, rotation_matrix):
+def generate_map(input_image_size, output_image_size, aperture, rotation_matrix, o_center_position):
     """
     This function generates a map for openCV's remap function.
 
@@ -169,14 +302,13 @@ def generate_map(input_image_size, output_image_size, aperture, rotation_matrix)
         print "error output_image_size"
     else:
         image_map = numpy.zeros((output_image_size[0],output_image_size[1],2),dtype = numpy.float32)
-        rotation_matrix = numpy.eye(3)
         for x in xrange(output_image_size[1]):
             for y in xrange(output_image_size[0]):
                 normal_x = lerp(x,0,output_image_size[1] - 1,-1,1)
                 normal_y = lerp(y,0,output_image_size[0] - 1,-1,1)
 
                 px,py,pz = equ_to_vector(normal_x, normal_y, r = 1000.0)
-                normal_fish_x, normal_fish_y = vector_to_fisheye(px,py,pz,aperture, 20/2.0,rotation_matrix)
+                normal_fish_x, normal_fish_y = vector_to_fisheye(px,py,pz,aperture, o_center_position,rotation_matrix)
 
                 fish_x = lerp(normal_fish_x, -1,1, 0, input_image_size[1] -1)
                 fish_y = lerp(normal_fish_y, -1,1, 0, input_image_size[0] -1)
@@ -185,7 +317,7 @@ def generate_map(input_image_size, output_image_size, aperture, rotation_matrix)
         return image_map
 
 
-def convert_fisheye_equ(input_image, output_image_size, aperture, rotation_matrix):
+def convert_fisheye_equ(input_image,output_image_name, output_image_size, aperture, rotation_matrix,o_center_position):
     """
     This function convert a equidistant projected fisheye lens into an equirectangular projection image.
 
@@ -199,18 +331,60 @@ def convert_fisheye_equ(input_image, output_image_size, aperture, rotation_matri
       matrix: unwarped image.
     """
     image = numpy.zeros((output_image_size[1],output_image_size[0],3),dtype = numpy.uint8)
-    image_map = generate_map(input_image.shape, image.shape, aperture, rotation_matrix)
+    image_map = generate_map(input_image.shape, image.shape, aperture, rotation_matrix,o_center_position)
 
     cv2.remap(src=input_image, dst=image, map1=image_map, map2=None,interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite("image.jpg",image)
+    cv2.imwrite(output_image_name,image)
     cv2.imshow("aaa",image)
     c = cv2.waitKey(0)
     return image
 
+def test_rigid_motion():
+    import random
+    points_a = numpy.array([[1,0,0],[0,1,0],[0,0,1]],dtype=numpy.float32)
+    points_b = numpy.array([[1,0,0],[0,1,0],[0,0,1]],dtype=numpy.float32)
+
+    #print math.pi / 2.0 * random.random()
+    rotation = rotation_matrix_x( math.pi*random.random())
+    for i in xrange(len(points_b)):
+        points_b[i] = rotation.dot(points_b[i])
+    
+
+    r,t = find_rigid_transformation(numpy.mat(points_b), numpy.mat(points_a))
+
+
+    A2 = (r*points_b.T) + numpy.tile(t, (1, 3))
+    A2 = A2.T
+
+    # Find the error
+    err = A2 - points_a
+    print rotation-r
+
+
+    print err
+    err = numpy.multiply(err, err)
+    err = numpy.sum(err)
+    rmse = numpy.sqrt(err/3.0)
+    print rmse
+
+    
+
+
 if __name__ == "__main__":
-    input_image = cv2.imread("fisheye_r.jpg")
-    #output_image = convert_fisheye_equ(input_image, (2048,1024), 200.0 / 180.0 * PI, None)
-    test_fisheye_to_equ()
+    input_image_r = cv2.imread("fisheye_r.jpg")
+    input_image_l = cv2.imread("fisheye_l.jpg")
+    #test_rigid_motion()
+    
+    
+    #test_fisheye_to_equ()
+    result, fov = dual_fisheye_calibrate(input_image_l,input_image_r,200 /180.0 *math.pi,200 /180.0 *math.pi)
+    print fov[0]/ math.pi*180,fov[1]/math.pi*180
+    error, r_l,t_l, r_r,t_r = result
+    print error
+    print r_l, t_l
+
+    output_image = convert_fisheye_equ(input_image_l,"out_l.jpg", (2048,1024), 200.0 / 180.0 * PI, r_l, t_l)
+    output_image = convert_fisheye_equ(input_image_r,"out_r.jpg", (2048,1024), 200.0 / 180.0 * PI, r_r, t_r)
 
 
 
