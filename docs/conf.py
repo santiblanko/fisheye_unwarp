8a6b49a4a68ab2a2fec96386855f87363bc987c3
Benson Chang <coolbenson.chang@gmail.com>
2018-09-27 17:55:06 -0700

    add matplotlib.pyplot

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['cv2',  'numpy','numpy.linalg']
+MOCK_MODULES = ['cv2',  'numpy','numpy.linalg',"matplotlib.pyplot"]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
